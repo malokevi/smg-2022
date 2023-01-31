df64fee943501399f18b9a79c5e43a57471f079d
Kevin Malone <malokevi@gmail.com>
2023-01-31 12:43:16 -0500

    Update index.tsx

--- src/components/Layout/Header/components/Navigation/index.tsx
+++ src/components/Layout/Header/components/Navigation/index.tsx
@@ -1,11 +1,11 @@
+import { useEffect, useRef, useState } from "react"
+import { Link, useLocation } from "react-router-dom"
 import clsx from "clsx"
-import { useState } from "react"
-import { Link } from "react-router-dom"
-import { useHeaderContext } from "../.."
 
 import Caret from '../../../../../assets/images/caret-down.svg'
 import NavData from "../../../../../static/navigation.json"
 import { Col, Row } from "../../../Grid"
+import { useHeaderContext } from "../.."
 
 import * as S from './styles'
 
@@ -22,9 +22,33 @@ interface LinkType extends Omit<DropdownLinkType, "to"> {
 export type NavType = LinkType[]
 
 export const Navigation = () => {
-    const { mobileNavIsOpen } = useHeaderContext()
+    const { mobileNavIsOpen, setMobileNavIsOpen } = useHeaderContext()
     const [visibleSubnav, setVisibleSubnav] = useState<number | undefined>()
     const navData: NavType = NavData
+    const navRef = useRef<HTMLUListElement | null>(null)
+    let location = useLocation()
+
+    // on route change, scroll to top and close navigation
+    useEffect(() => {
+        handleSetVisibleSubnav()
+        setMobileNavIsOpen(false)
+        window.scrollTo(0, 0)
+    }, [location]);
+
+    // close subnav when clicking outside of navigation
+    useEffect(() => {
+        const handleClickOutside = ({ target }: MouseEvent): void => {
+            if (navRef.current && !navRef.current.contains(target as Node)) {
+                handleSetVisibleSubnav()
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside, true)
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside, true)
+        };
+    }, []);
 
     const handleSetVisibleSubnav = (index?: number) => {
         setVisibleSubnav(index === visibleSubnav ? undefined : index)
