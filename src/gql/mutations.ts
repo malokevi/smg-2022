4261256d1bd84f6ce6726a7b9422f865b1c3256b
Kevin Malone <malokevi@gmail.com>
2023-01-31 22:23:36 -0500

    add gql mutations

--- src/gql/mutations.ts
+++ src/gql/mutations.ts
@@ -1,1 +1,21 @@
-export const getProductsService = async (): Promise<any> => {}
+import { gql } from "@apollo/client"
+
+export const REGISTER = gql(`
+    mutation Register($username: string!, $email: string!, $password: string!) {
+        register(input: { username: $username, email: $email, password: $password }) {
+            jwt
+            user {
+                username
+                email
+            }
+        }
+    }
+`)
+
+export const LOGIN = gql(`
+    mutation Login($identifier: string!, $password: string!) {
+        login($identifier: $identifier, password: $password }) {
+            jwt
+        }
+    }
+`)
